456ccbd969c3ed66960a2edbb97235c96b198152
csimal <cedric.simal@student.unamur.be>
2020-03-28 22:15:36 +0100

    IT'S ALIVE!

--- src/NetworkEpidemics.jl
+++ src/NetworkEpidemics.jl
@@ -7,22 +7,26 @@ using LightGraphs
 using LightGraphs.SimpleGraphs
 
 
-include("gillespie.jl")
-include("meanfield.jl")
 include("utils\\rand.jl")
 include("utils\\categorical_tree.jl")
 
 include("models\\AbstractEpidemicModel.jl")
+export AbstractEpidemicModel
+
 include("models\\models\\metapopulation.jl")
 include("models\\models\\metaplex.jl")
 include("models\\models\\contact_process.jl")
+
+
+include("gillespie.jl")
+include("meanfield.jl")
+
+export gillespie, meanfield
 
 include("models\\dynamics\\SI.jl")
 include("models\\dynamics\\SIS.jl")
 include("models\\dynamics\\SIR.jl")
 
-export AbstractEpidemicModel
-export gillespie, meanfield
 export Metapopulation, ContactProcess, Metaplex
 export SI, SIS, SIR
 
